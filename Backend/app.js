0408b918a7b95a7daf01f006c2492f5ab25f0af9
Belloir Sébastien <sebastien.belloir@gmail.com>
2020-08-12 16:48:14 +0200

    add new branch + bunch of other stuff
    created controllers for users

--- Backend/app.js
+++ Backend/app.js
@@ -1,9 +1,8 @@
+const dotenv = require('dotenv');
+dotenv.config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const dotenv = require('dotenv');
-dotenv.config();
-const userRoutes = require('./routes/user');
 const mysqlConnection = require('./mysqlConnection');
 
 const app = express();
--- Backend/controllers/user.js
+++ Backend/controllers/user.js
@@ -1,43 +1,81 @@
 const mysqlConnection = require('../mysqlConnection');
-const brcrypt = require('bcrypt'); 
+const brcrypt = require('bcrypt');
 const jsonWebToken = require('jsonwebtoken');
 
 
-exports.signup = (req, res, next) => { 
-    brcrypt.hash(req.body.password, 10) 
-    .then(hash => { 
-        const user = new User({
-            email: req.body.email,
-            password: hash
-        });
-        user.save()
-        .then(() => res.status(201).json({message: 'Votre compte a été créé !'}))
-        .catch(error => res.status(400).json({error}));
-    })
-    .catch(error => res.status(500).json({error}));
-};
-
-exports.login = (req, res, next) => { // Connection à un compte déjà existant
-    User.findOne({email: req.body.email})
-    .then(user => {
-        if (!user) {
-            return res.status(401).json({error: 'Utilisateur non trouvé !'});
-        }
-        brcrypt.compare(req.body.password, user.password) // comparaison du mot de passe entré par l'utilisateur avec le hash enregistré dans la BDD.
-        .then(valid => {
-            if (!valid) {
-                return res.status(401).json({error: 'Mot de passe incorrect !'}); 
+exports.signup = (req, res, next) => {
+    const user = req.body
+    bcrypt.hash(user.password, 10).then((hash) => {
+        user.password = hash
+        mysqlConnection.query('INSERT INTO users SET ?', user, function (
+            error,
+            results,
+            fields
+        ) {
+            if (error) {
+                console.log(error)
+                return res.status(400).json(error.sqlMessage)
             }
-            res.status(200).json({ // Envoi de la réponse contenant l'id utilisateur + token
-                userId: user._id,
-                token: jsonWebToken.sign(
-                    { userId: user._id },
-                    'hLq^hNoHp{Vw)c`HWHGwAq#vm&)1nX', // clé d'encodage
-                    { expiresIn: '24h'} // Date d'expiration du TOKEN
-                )
-            });
+            return res.status(201).json({
+                message:
+                    'Compte créé avec succès'
+            })
         })
-        .catch(error => res.status(500).json({error}));
     })
-    .catch(error => res.status(500).json({error}));
-};
+}
+
+exports.login = (req, res, next) => {
+    const userReq = req.body.username
+    const passReq = req.body.password
+    if (userReq && passReq) {
+        conn.query(
+            'SELECT * FROM groupomania.users WHERE username= ?',
+            userReq,
+            function (_error, results, _fields) {
+                if (results.length > 0) {
+                    bcrypt.compare(passReq, results[0].password).then((valid) => {
+                        if (!valid) {
+                            res
+                                .status(401)
+                                .json({ message: 'Utilisateur ou mot de passe inconnu' })
+                        } else {
+                            console.log(userReq, "s'est connecté")
+                            res.status(200).json({
+                                userId: results[0].idUser,
+                                email: results[0].Email,
+                                token: jsonWebToken.sign(
+                                    { userId: results[0].idUser },
+                                    'hLq^hNoHp{Vw)c`HWHGwAq#vm&)1nX',
+                                    { expiresIn: '24h' }
+                                )
+                            })
+                        }
+                    })
+                } else {
+                    res
+                        .status(401)
+                        .json({ message: 'Utilisateur ou mot de passe inconnu' })
+                }
+            }
+        )
+    } else {
+        res
+            .status(500)
+            .json({ message: "Entrez un nom d'utilisateur et un mot de passe" })
+    }
+}
+
+exports.deleteUser = (req, res, next) => {
+    mysqlConnection.query(
+        `DELETE FROM users WHERE idUSERS=${req.params.id}`,
+        req.params.id,
+        function (error, results, fields) {
+            if (error) {
+                return res.status(400).json(error)
+            }
+            return res
+                .status(200)
+                .json({ message: 'Votre compte a bien été supprimé !' })
+        }
+    )
+}
