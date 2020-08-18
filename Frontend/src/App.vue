8db4c741c8b33f603d1b0d7d3a299e88cc50e996
Belloir Sébastien <sebastien.belloir@gmail.com>
2020-08-18 08:45:40 +0200

    Test vue

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -1,32 +1,32 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <Nav />
+    <SignIn />
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+<script>
+import SignIn from './components/SignIn'
+import Nav from './components/Nav'
+export default {
+  name: 'app',
+  components: {
+    SignIn,
+    Nav
   }
 }
+</script>
+
+<style lang="scss">
+  body {
+    margin: 0;
+    padding: 0;
+  }
+  #app {
+    font-family: Tahoma;
+    font-size: 1.6rem;
+    color: #222;
+    background-color: #FFF;
+    height: 100vh;
+  }
 </style>
